a11b0fc89ad5b6c63e64dea1e668e63c9cb4fa31
Niky222 <86951612+Niky222@users.noreply.github.com>
2025-10-02 16:41:59 +0300

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -35,6 +35,7 @@ def upload(social, video_path, title):
 if __name__ == "__main__":
     new_videos = [v for v in get_all_video_ids() if v["status"] == "new"]
     main_video_title = new_videos[0]['title']
+    video_id = new_videos[0]['video_id']
     upload(social ="tiktok", video_path="res.mp4", title=main_video_title)
     update_video_status(video_id, "downloaded", "videos.db")
 
