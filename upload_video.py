19f5ff8c7809a09ccf86765e6401d33fcfcc808f
baobaoochinese-cyber <baobaoochinese@gmail.com>
2025-10-13 09:02:55 +0300

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -2,7 +2,7 @@ import requests
 import subprocess
 import json
 import feedparser
-from sql import init_db, save_video_id, get_all_video_ids, update_video_status
+from sql import init_db, save_video_id, get_all_video_ids, update_video_status, show_all
 
 
 API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5pa2l0YWQ1ODkwQGdtYWlsLmNvbSIsImV4cCI6NDkxMTc4ODU2MywianRpIjoiOTc2MjM1ZjctZDEwMy00NDgxLWI2NzItODQ4OWFkODgyYzlhIn0.FgyGGk3ymOnA_4GwKTwVFvA2dc0h2NFm0TCcxbE9ntw"
@@ -36,6 +36,9 @@ if __name__ == "__main__":
     new_videos = [v for v in get_all_video_ids() if v["status"] == "new"]
     main_video_title = new_videos[0]['title']
     video_id = new_videos[0]['video_id']
+    show_all()
+    print(main_video_title)
     upload(social ="tiktok", video_path="res.mp4", title=main_video_title)
     update_video_status(video_id, "downloaded", "videos.db")
+    show_all()
 
